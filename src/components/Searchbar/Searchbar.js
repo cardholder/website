ffc0a1620582bfe80123ca7330cd725f08a7db40
Patrick <patrick.reinke@fh-bielefeld.de>
2019-05-16 21:06:42 +0200

    :lipstick: set up searchbar

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Input } from "reactstrap";
+
+import classes from "./Searchbar.css";
+
+const searchbar = () => (
+  <div className={ classes.Searchbar }>
+    <Input></Input>
+  </div>
+);
+
+export default searchbar;
