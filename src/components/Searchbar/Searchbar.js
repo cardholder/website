fc605ee44ea0d4878a744368cd1866b74701f572
Patrick <patrick.reinke@fh-bielefeld.de>
2019-05-17 15:10:07 +0200

    :lipstick: update searchbar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Input } from "reactstrap";
 
+import searchIcon from '../../assets/search.svg';
+
 import classes from "./Searchbar.css";
 
 const searchbar = () => (
   <div className={classes.Searchbar}>
-    <Input />
+    <Input style={{ backgroundImage: "url(" + { searchIcon } + ")" }} />
   </div>
 );
 
