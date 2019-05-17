8c253297e7003f209f64ee6fc66aea62d75bf5b7
Patrick <patrick.reinke@fh-bielefeld.de>
2019-05-17 10:17:29 +0200

    :lipstick: add hover animation

--- src/containers/Lobbylist/LobbylistItems/LobbylistItems.js
+++ src/containers/Lobbylist/LobbylistItems/LobbylistItems.js
@@ -1,110 +1,55 @@
 import React from "react";
 import { Container, Row } from "reactstrap";
+import posed from 'react-pose';
 import LobbylistItem from "./LobbylistItem/LobbylistItem";
 
 import classes from "./LobbylistItems.css";
 
-const lobbylistItems = () => (
-  <Container>
-    <Row className={classes.Lobbylist}>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-      <div className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
-        <LobbylistItem
-          id={"dfjs3"}
-          game={"Mau-Mau"}
-          maxPlayers={4}
-          actualPlayers={2}
-        />
-      </div>
-    </Row>
-  </Container>
-);
+const lobbylistItems = () => {
+  const Box = posed.div({
+    hoverable: true,
+    pressable: true,
+    init: {
+      scale: 1
+    },
+    hover: {
+      scale: 1.05
+    },
+    press: {
+      scale: 1.025
+    }
+  });
+
+  return (
+    <Container>
+      <Row className={classes.Lobbylist}>
+        <Box className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
+          <LobbylistItem
+            id={"dfjs3"}
+            game={"Mau-Mau"}
+            maxPlayers={4}
+            actualPlayers={2}
+          />
+        </Box>
+        <Box className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
+          <LobbylistItem
+            id={"dfjs3"}
+            game={"Mau-Mau"}
+            maxPlayers={4}
+            actualPlayers={2}
+          />
+        </Box>
+        <Box className={[classes.LobbylistItems, "col-sm-6"].join(" ")}>
+          <LobbylistItem
+            id={"dfjs3"}
+            game={"Mau-Mau"}
+            maxPlayers={4}
+            actualPlayers={2}
+          />
+        </Box>
+      </Row>
+    </Container>
+  );
+};
 
 export default lobbylistItems;
