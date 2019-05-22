576469fdf30a3a6a6cbba9477d14ed574abe7de9
Patrick <patrick.reinke@fh-bielefeld.de>
2019-05-22 16:56:41 +0200

    :fire: remove websocket test

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,53 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Input } from "reactstrap";
-import Websocket from "react-websocket";
 
 import searchIcon from "../../assets/search.svg";
 
 import classes from "./Searchbar.css";
 
-class Searchbar extends Component {
-  state = {
-    url:
-      "ws://ec2-18-195-20-221.eu-central-1.compute.amazonaws.com:8000/ws/chat/lobby/"
-  };
+const searchbar = () => {
+  return (
+    <div className={classes.Searchbar}>
+      <Input
+        style={{ backgroundImage: "url(" + { searchIcon } + ")" }}
+        onKeyPress={event => {
+          if (event.key === "Enter") {
+            this.sendMessage("React ist besser!");
+          }
+        }}
+      />
+    </div>
+  );
+};
 
-  handleData(data) {
-    console.log(data);
-  }
-
-  sendMessage(message) {
-    console.log(message);
-    this.refWebSocket.sendMessage(
-      JSON.stringify({
-        message: message
-      })
-    );
-  }
-
-  render() {
-    return (
-      <div className={classes.Searchbar}>
-        <Input
-          style={{ backgroundImage: "url(" + { searchIcon } + ")" }}
-          onKeyPress={event => {
-            if (event.key === "Enter") {
-              this.sendMessage("Test 123");
-            }
-          }}
-        />
-        <Websocket
-          url={this.state.url}
-          onMessage={this.handleData}
-          onOpen={console.log("Open")}
-          onClose={console.log("Closed")}
-          ref={Websocket => {
-            this.refWebSocket = Websocket;
-          }}
-        />
-      </div>
-    );
-  }
-}
-
-export default Searchbar;
+export default searchbar;
