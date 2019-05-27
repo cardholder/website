7a4b64fc290873300e103c45135c49c069d50ee8
Patrick <patrick.reinke@fh-bielefeld.de>
2019-05-27 14:28:28 +0200

    :white_check_mark: add sideDrawer tests

---
+++ src/components/Navigation/SideDrawer/SideDrawer.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import SideDrawer from "./SideDrawer";
+import Title from "../../Title/Title";
+import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Footer from "../../Footer/Footer";
+
+configure({ adapter: new Adapter() });
+
+describe("<SideDrawer />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<SideDrawer />);
+  });
+
+  it("should render a <Title /> without link", () => {
+    expect(wrapper.find(Title)).toHaveLength(1);
+  });
+
+  it("should render a <Title /> with <NavigationItem />", () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(1);
+  });
+
+  it("should render <NavigationItems />", () => {
+    expect(wrapper.find(NavigationItems)).toHaveLength(1);
+  });
+
+  it("should render a <Footer />", () => {
+    expect(wrapper.find(Footer)).toHaveLength(1);
+  });
+});
