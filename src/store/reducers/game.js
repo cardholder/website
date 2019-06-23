de64d91916da5682c9c384742f94f1a70a880779
Patrick Reinke <patrick.reinke@fh-bielefeld.de>
2019-06-23 11:36:02 +0200

    :fire: remove redundant code

--- src/store/reducers/game.js
+++ src/store/reducers/game.js
@@ -46,12 +46,6 @@ const onMessage = (state, action) => {
     });
   }
 
-  if (data.card) {
-    modifiedState = updateObject(modifiedState, {
-      top_card_of_discard_pile: data.card
-    });
-  }
-
   if (data.cards_drawn) {
     let cards = [...state.cards];
 
